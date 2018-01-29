0a86ca6d7cbc08e276693040029ee9c63ae834d5
Javier Luna <javier.luna@orbitalads.io>
2018-01-29 10:31:37 +0100

    Fix setup.py file. Add setup.cfg for markdown support at PyPI

--- setup.py
+++ setup.py
@@ -9,14 +9,13 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='nanoprobe',
     version='0.0.1',
     description='Set of tools to profile your shiny python code',
-    long_description=long_description,
     url='https://github.com/OrbitalAds/nanoprobe',
     author='OrbitalAds',
     author_email='dev@orbitalads.io',
